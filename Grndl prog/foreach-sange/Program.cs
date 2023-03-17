bc0b833e7b956f17efdd205abe9bce405fd95599
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-03-17 13:38:53 +0100

    fixed the error in the new project "foreach-sange"

---
+++ Grndl prog/foreach-sange/Program.cs
@@ -0,0 +1,36 @@
+﻿//XXXX skal udskiftes med den korrekte kode
+
+namespace sange
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+
+            string[] Sange = { "Old time road", "Paris", "SOS", "So Am I" };
+
+
+
+            Console.WriteLine("Her er mine ynglingssange");
+
+
+
+            foreach (string s in Sange)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+
+}
