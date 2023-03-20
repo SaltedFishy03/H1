f2be5324b0fea990af45ea6565285112f43122c7
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-03-20 09:27:30 +0100

    finnished and fixed all errors in project "Logiske operatore"

--- Grndl prog/Logiske operatorer 1/Program.cs
+++ Grndl prog/Logiske operatorer 1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+
+using System.Text;
+using System.Xml.Linq;
+
+namespace and_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name, password;
+
+            name = "Steven";
+            password = "Steven123";
+
+            // evaluating both expresson and returns true if              all are true.
+
+            if (name == "Steven" && password == "Steven123")
+            {
+                Console.WriteLine("login succesful");
+            }
+            else
+            {
+                Console.WriteLine("Unauthorised access");
+            }
+            Console.ReadLine();
+        }
+    }
+}
